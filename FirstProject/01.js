b9d8d590d4e529a81829ab49925664e857cdce72
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2024-03-19 15:45:02 +0200

    my rep

---
+++ FirstProject/01.js
@@ -0,0 +1,8 @@
+//npm istall -g typescript
+//tsc --h
+//tsc --init
+var x = 5;
+var num = 20;
+x = true;
+console.log(num);
+var mess = 'Hello';
---
+++ FirstProject/01.ts
@@ -0,0 +1,13 @@
+//npm istall -g typescript
+//tsc --h
+//tsc --init
+
+const x = 5;
+
+let num: number = 20;
+
+x = true;
+
+console.log(num);
+
+let mess: string = 'Hello';
